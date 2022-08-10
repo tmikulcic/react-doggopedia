514c6674d6c52a2f0970e7a6523a4a7dcf27002c
Tomislav <tom.mikulcic@gmail.com>
2022-08-10 23:55:48 +0200

    Doggo data is now shown with props + .map method

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-export default function Card() {
+export default function Card(props) {
   return (
     <div className='card'>
       <img
         className='card-image'
-        src='./images/doggos/samoyed.jpg'
-        alt='doggo image'
+        src={`.${props.item.img}`}
+        alt={props.item.breed}
       ></img>
-      <div className='card-name'>Samoyed</div>
+      <div className='card-name'>{props.item.breed}</div>
     </div>
   );
 }
--- src/components/ContentView.js
+++ src/components/ContentView.js
@@ -3,19 +3,9 @@ import Card from './Card';
 import data from './data';
 
 export default function ContentView() {
-  return (
-    <div className='content-view'>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-    </div>
-  );
+  const cards = data.map((item) => {
+    return <Card key={item.id} item={item} />;
+  });
+
+  return <div className='content-view'>{cards}</div>;
 }
