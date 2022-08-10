c903f645bd6d57783d452e1010dbf655512452cb
Tomislav <tom.mikulcic@gmail.com>
2022-08-10 22:19:03 +0200

    Add SearchFilter layout and initial styling

--- src/components/SearchFilter.js
+++ src/components/SearchFilter.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
 export default function SearchFilter() {
-  return <div className='search-filter'>SearchFilter</div>;
+  return (
+    <div className='search-filter'>
+      <button className='filter-button'>Filter</button>
+      <input className='search-window' type='text'></input>
+      <button className='search-button'>Search</button>
+    </div>
+  );
 }
