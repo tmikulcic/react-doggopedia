0fc7c439e2a97aa787b688a5b3fadd4dd375ec51
Tomislav <tom.mikulcic@gmail.com>
2022-08-10 23:40:22 +0200

    Fix content view scrolling and card scaling

--- src/components/ContentView.js
+++ src/components/ContentView.js
@@ -7,6 +7,14 @@ export default function ContentView() {
       <Card />
       <Card />
       <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
     </div>
   );
 }
