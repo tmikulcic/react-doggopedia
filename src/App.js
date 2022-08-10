0a2040130f30b559fe7ad8b5f375b1d5bd785137
Tomislav <tom.mikulcic@gmail.com>
2022-08-10 21:11:47 +0200

    Containers layout added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import Navbar from './Navbar';
+import SearchFilter from './components/SearchFilter';
+import ContentView from './components/ContentView';
+
 function App() {
-  return <div></div>;
+  return (
+    <div className='main-container'>
+      <div className='left-side'>
+        <Navbar />
+      </div>
+      <div className='right-side'>
+        <SearchFilter />
+        <ContentView />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/ContentView.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function ContentView() {
+  return <div className='content-view'>ContentView</div>;
+}
