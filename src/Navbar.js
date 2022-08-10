a06cf6ed9fa7a0c79c6255ac7fe7740554b4390f
Tomislav <tom.mikulcic@gmail.com>
2022-08-10 21:46:36 +0200

    Add Navbar layout and initial styling

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,19 @@
 import React from 'react';
 
 export default function Navbar() {
-  return <div className='navbar'>Navbar</div>;
+  return (
+    <div className='navbar'>
+      <div className='logo'>
+        <img
+          className='logo-picture'
+          src='./images/logo.png'
+          alt='logo shiba'
+        ></img>
+        <div className='logo-text'>Doggopedia</div>
+      </div>
+      <div className='nav-elements'>Home</div>
+      <div className='nav-elements'>About</div>
+      <div className='nav-elements'>Contact</div>
+    </div>
+  );
 }
