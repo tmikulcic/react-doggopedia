ecd9fc02b518e8386e35c46ac3adc2c4b605ac49
Tomislav <tom.mikulcic@gmail.com>
2022-08-10 23:49:19 +0200

    Add data.js

--- src/components/ContentView.js
+++ src/components/ContentView.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Card from './Card';
+import data from './data';
 
 export default function ContentView() {
   return (
---
+++ src/components/data.js
@@ -0,0 +1,31 @@
+export default [
+  {
+    id: 1,
+    breed: 'Golden Retriever',
+    img: './images/doggos/golden-retriever.jpg',
+  },
+
+  {
+    id: 2,
+    breed: 'Labrador',
+    img: './images/doggos/labrador.jpg',
+  },
+
+  {
+    id: 3,
+    breed: 'Rottweiler',
+    img: './images/doggos/rottweiler.jpg',
+  },
+
+  {
+    id: 4,
+    breed: 'Samoyed',
+    img: './images/doggos/samoyed.jpg',
+  },
+
+  {
+    id: 5,
+    breed: 'Schnauzer',
+    img: './images/doggos/schnauzer.jpg',
+  },
+];
