01c53213ab110df31e702cb01fa3d0dd9cfa269b
Tomislav <tom.mikulcic@gmail.com>
2022-08-13 13:39:48 +0200

    Add modal window, needs work on styling

--- src/components/Home/Card.js
+++ src/components/Home/Card.js
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Card.css';
 
 export default function Card(props) {
+  const [modal, setModal] = useState(false);
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
+  if (modal) {
+    document.body.classList.add('active-modal');
+  } else {
+    document.body.classList.remove('active-modal');
+  }
+
   return (
     <div className='card'>
       <img
         className='card-image'
         src={`.${props.item.img}`}
         alt={props.item.breed}
+        onClick={toggleModal}
       ></img>
-      <div className='card-name'>{props.item.breed}</div>
+      <div className='card-name' onClick={toggleModal}>
+        {props.item.breed}
+      </div>
+
+      {/* Modal */}
+      {modal && (
+        <div className='modal'>
+          <div className='overlay' onClick={toggleModal}>
+            <div className='modal-content'>
+              <img
+                className='card-image'
+                src={`.${props.item.img}`}
+                alt={props.item.breed}
+                onClick={toggleModal}
+              ></img>
+              <div className='card-name' onClick={toggleModal}>
+                {props.item.breed}
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- src/components/Home/CardModal.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function CardModal() {
-  return <div>CardModal</div>;
-}
