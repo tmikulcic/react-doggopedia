8b86b39dfdc01a49e2287ec3c6dc140601708f6e
Tomislav <tom.mikulcic@gmail.com>
2022-08-13 12:49:48 +0200

    Add Contact and About page

--- src/components/Home/About/About.js
+++ src/components/Home/About/About.js
@@ -2,5 +2,24 @@ import React from 'react';
 import './About.css';
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className='info-container'>
+      <h1 className='header'>About page</h1>
+      <p className='paragraph'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque error
+        cum dolore officiis repellat quas eos ipsam at, excepturi laudantium
+        repudiandae facilis dolorem aspernatur alias expedita maiores fugiat
+        dolores nesciunt. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Cumque error cum dolore officiis repellat quas eos ipsam at,
+        excepturi laudantium repudiandae facilis dolorem aspernatur alias
+        expedita maiores fugiat dolores nesciunt.
+      </p>
+      <p className='paragraph'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque error
+        cum dolore officiis repellat quas eos ipsam at, excepturi laudantium
+        repudiandae facilis dolorem aspernatur alias expedita maiores fugiat
+        dolores nesciunt.
+      </p>
+    </div>
+  );
 }
--- src/components/Home/Contact/Contact.js
+++ src/components/Home/Contact/Contact.js
@@ -1,6 +1,35 @@
 import React from 'react';
+import '../About/About.css';
 import './Contact.css';
 
 export default function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div className='info-container'>
+      <div className='header'>Contact</div>
+      <div className='paragraph'>
+        You can follow my work and check for other projects here:
+      </div>
+
+      <a href='https://github.com/tmikulcic'>
+        <img
+          className='contact-logo'
+          src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg'
+        />
+      </a>
+      <div className='paragraph'>Check me out on Social Media:</div>
+      <a href='https://facebook.com'>
+        <img
+          className='contact-logo'
+          src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/facebook/facebook-original.svg'
+        />
+      </a>
+      <a href='https://twitter.com'>
+        <img
+          className='contact-logo'
+          src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/twitter/twitter-original.svg'
+        />
+      </a>
+      <a href=''></a>
+    </div>
+  );
 }
