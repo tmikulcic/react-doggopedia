a85dd65b16f66407a80b3412659eda28045632a2
Tomislav <tom.mikulcic@gmail.com>
2022-08-14 09:31:15 +0200

    Add Filter layout and styling

--- src/components/Home/SearchFilter.js
+++ src/components/Home/SearchFilter.js
@@ -9,14 +9,58 @@ export default function SearchFilter({ setSearchTerm }) {
 
   return (
     <div className='search-filter'>
-      <button className='filter-button'>Filter</button>
-      <input
-        className='search-window'
-        type='text'
-        placeholder='Type dog breed...'
-        onChange={searchChange}
-      ></input>
-      <button className='search-button'>Search</button>
+      <div className='search'>
+        <button className='filter-button'>Filter</button>
+        <input
+          className='search-window'
+          type='text'
+          placeholder='Type dog breed...'
+          onChange={searchChange}
+        ></input>
+        <button className='search-button'>Search</button>
+      </div>
+      <div className='filter'>
+        <input
+          className='filter-radio'
+          type='radio'
+          id='All'
+          value='All'
+          name='size'
+        />{' '}
+        <label for='All' type='text' name='All' checked='true'>
+          All
+        </label>
+        <input
+          className='filter-radio'
+          type='radio'
+          id='Small'
+          value='Small'
+          name='size'
+        />{' '}
+        <label for='Small' type='text' name='Small'>
+          Small
+        </label>
+        <input
+          className='filter-radio'
+          type='radio'
+          id='Medium'
+          value='Medium'
+          name='size'
+        />{' '}
+        <label for='Medium' type='text' name='Medium'>
+          Medium
+        </label>
+        <input
+          className='filter-radio'
+          type='radio'
+          id='Large'
+          value='Large'
+          name='size'
+        />{' '}
+        <label for='Large' type='text' name='Large'>
+          Large
+        </label>
+      </div>
     </div>
   );
 }
